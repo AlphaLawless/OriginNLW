9de2d8a0dd826a3a6e6725c1c491285c0258e9d9
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2021-09-15 02:37:28 -0300

    update: create the link active and functions for scrolls

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ for (const link of links) {
 const header = document.querySelector('#header');
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
   } else {
     header.classList.remove('scroll');
   }
-});
+};
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
@@ -32,7 +32,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  Keyboard: true
+  Keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 });
 
 const scrollReveal = ScrollReveal({
@@ -51,3 +57,28 @@ scrollReveal.reveal(`
   footer .brand, footer .social
   `, {interval: 100}
 );
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+    } else {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  activateMenuAtCurrentSection()
+  changeHeaderWhenScroll()
+});
